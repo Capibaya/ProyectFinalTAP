9e16fc752a3cbebb0fb5d9c4ffc699ff414f7194
Roberto Moreno Rodriguez <refrymanjr@gmail.com>
2026-05-24 21:51:29 -0600

    interfaz generica dao

---
+++ src/main/java/com/hotel/DAO/GenericDAO.java
@@ -0,0 +1,15 @@
+package com.hotel.DAO;
+
+
+import java.util.List;
+/**
+ * Interfaz generica DAO con operaciones CRUD.
+ * @param <T> Tipo de entidad
+ */
+public interface GenericDAO<T> {
+    void guardar(T entidad);
+    T buscarPorId(int id);
+    List<T> obtenerTodos();
+    void actualizar(T entidad);
+    void eliminar(int id);
+}
